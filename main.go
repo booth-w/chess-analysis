5e298f8ce1f06a083b626566bb483341222d7313
booth-w <personal@booth-w.co.uk>
2026-07-13 19:19:50 +0100

    Only check for winner on metadata lines

--- main.go
+++ main.go
@@ -14,8 +14,6 @@ func main() {
 	flagProfile := flag.Bool("profile", false, "Enable CPU profiling (creates cpu.prof)")
 	flag.Parse()
 
-	fmt.Println(*flagProfile)
-
 	if *flagProfile {
 		profFile, _ := os.Create("cpu.prof")
 		pprof.StartCPUProfile(profFile)
@@ -24,16 +22,41 @@ func main() {
 
 	s := bufio.NewScanner(os.Stdin)
 
-	total := 0
+	game := [2][]string{}
+	totalGames := 0
 	wins := [3]int{}
+
+	// itterate over each line
 	for s.Scan() {
 		line := s.Text()
+		if len(line) != 0 {
+			if line[0] == '[' {
+				game[0] = append(game[0], line)
+			} else {
+				game[1] = append(game[1], line)
+			}
+			continue
+		} else if len(game[1]) == 0 {
+			continue
+		}
 
-		winner := parser.GetWinner(line)
-		if (winner != -1) {
-			total++
+		winner := parser.GetWinner(game[0])
+		if winner != -1 {
+			totalGames++
 			wins[winner]++
 		}
+		game = [2][]string{}
 	}
-	fmt.Printf("White: %d\nBlack: %d\nDraw:  %d\nTotal: %d\n", wins[0], wins[1], wins[2], total)
+
+	whitePercent := float64(wins[0]) / float64(totalGames) * 100
+	blackPercent := float64(wins[1]) / float64(totalGames) * 100
+	drawPercent := float64(wins[2]) / float64(totalGames) * 100
+
+	fmt.Printf(
+		"White: %d (%.2f%%)\nBlack: %d (%.2f%%)\nDraw:  %d (%.2f%%)\nTotal: %d\n",
+		wins[0], whitePercent,
+		wins[1], blackPercent,
+		wins[2], drawPercent,
+		totalGames,
+	)
 }
--- pkg/parser/get-winner.go
+++ pkg/parser/get-winner.go
@@ -1,13 +1,15 @@
 package parser
 
-func GetWinner(line string) (int) {
-	if len(line) >= 2 && line[1] == 'R' {
-		if line[10] == '/' {
-			return 2 // draw
-		} else if line[9] == '1' {
-			return 0 // white
-		} else {
-			return 1 // black
+func GetWinner(game []string) int {
+	for _, line := range game {
+		if len(line) >= 2 && line[1] == 'R' {
+			if line[10] == '/' {
+				return 2 // draw
+			} else if line[9] == '1' {
+				return 0 // white
+			} else {
+				return 1 // black
+			}
 		}
 	}
 	return -1
