5d123d44025950afc1ca33f51b6d12ff4a826215
booth-w <personal@booth-w.co.uk>
2026-07-21 01:26:33 +0100

    Add log level flag

--- main.go
+++ main.go
@@ -10,10 +10,43 @@ import (
 	"github.com/booth-w/chess-analysis/pkg/parser"
 )
 
+func parseLogLevel(level string) slog.Level {
+	switch level {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	case "":
+		return slog.Level(99)
+	default:
+		return slog.LevelInfo
+	}
+}
+
 func main() {
 	flagElo := flag.String("elo", "", "Elo rating to filter games by. Usage: <min> or <min>-<max> (inclusive). Example: 1500 or 1500-2000")
 	flagProfile := flag.Bool("profile", false, "Enable CPU profiling (creates cpu.prof)")
+	flagLogLevel := flag.String("log-level", "", "Set the log level. Usage: debug, info, warn, error")
 	flag.Parse()
+
+	logLevel := parseLogLevel(*flagLogLevel)
+
+	if *flagLogLevel != "" && logLevel == slog.LevelInfo {
+		slog.Warn("Invalid log level. Defaulting to info", "level", *flagLogLevel)
+	}
+
+	slog.SetDefault(slog.New(
+		slog.NewTextHandler(
+			os.Stderr,
+			&slog.HandlerOptions{
+				Level: logLevel,
+			},
+		),
+	))
 
 	if *flagProfile {
 		profFile, _ := os.Create("cpu.prof")
